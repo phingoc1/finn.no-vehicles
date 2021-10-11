c826368dce37575fc59db655c589594519fb85c1
Phi <phingoc88@hotmail.com>
2021-10-11 12:52:55 +0200

    added tittel to json

--- index.php
+++ index.php
@@ -12,14 +12,18 @@ $document = new Document('https://www.finn.no/pw/search/snowmobile?orgId='.$_ENV
 $ads = $document->find('.unit.flex.align-items-stretch.result-item');
 
 foreach($ads as $ad) {
+    $tekst = $ad->find('.t4.word-break.mhn')[0]->text();
+    $tittel = explode(" ", $tekst);
+
     $master[] = array(  
-        'link' => $post->find('a.clickable::attr(href)')[0], 
-        'bilde' => $post->find('img.centered-image::attr(src)')[0], 
-        'tekst' => $post->find('.t4.word-break.mhn')[0]->text(), 
-        'sted' => $post->find('.blockify.ptt.licorice.fleft.mrm')[0]->text(),
-        'aar' => (strlen($post->find('.t5.word-break.mhn')[0]->firstChild()->text() ) == 4) ? $post->find('.t5.word-break.mhn')[0]->firstChild()->text() : "0000",
-        'km' => strpos($post->find('.t5.word-break.mhn')[0]->child(1)->text(), "km") ? $post->find('.t5.word-break.mhn')[0]->child(1)->text() : "0",
-        'pris' => strpos($post->find('.t5.word-break.mhn')[0]->lastChild()->text(), ",-") ? $post->find('.t5.word-break.mhn')[0]->lastChild()->text() : "0"
+        'link' => $ad->find('a.clickable::attr(href)')[0], 
+        'tittel' => $tittel[0]. " " . $tittel[1],
+        'bilde' => $ad->find('img.centered-image::attr(src)')[0], 
+        'tekst' => $tekst, 
+        'sted' => $ad->find('.blockify.ptt.licorice.fleft.mrm')[0]->text(),
+        'aar' => (strlen($ad->find('.t5.word-break.mhn')[0]->firstChild()->text() ) == 4) ? $ad->find('.t5.word-break.mhn')[0]->firstChild()->text() : "0000",
+        'km' => strpos($ad->find('.t5.word-break.mhn')[0]->child(1)->text(), "km") ? $ad->find('.t5.word-break.mhn')[0]->child(1)->text() : "0",
+        'pris' => strpos($ad->find('.t5.word-break.mhn')[0]->lastChild()->text(), ",-") ? $ad->find('.t5.word-break.mhn')[0]->lastChild()->text() : "0"
     );
 }
 
